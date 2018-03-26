f6b53b1f7fb6ece63cef38440d3e058a45e961f8
Mark Gil Libres <markglibres@gmail.com>
2018-03-27 07:27:39 +1100

    setup umbraco

---
+++ src/MBran.BusinessHours/App_Plugins/MBran.BusinessHours/js/businesshours.controller.js
@@ -0,0 +1,22 @@
+﻿angular.module('umbraco')
+    .controller('MBran.BusinessHours.BusinessHoursController', function ($scope) {
+
+        function init() {
+            initOptions();
+            initModel();
+        }
+
+        init();
+
+
+        function initOptions() {
+            $scope.model.hideLabel = true;
+
+        }
+
+        function initModel() {
+            
+        }
+
+        console.log('test');
+    });
