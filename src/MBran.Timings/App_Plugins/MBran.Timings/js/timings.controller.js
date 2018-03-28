b57fef6d2d268884c2d8646e964483b21ce32e50
Mark Gil Libres <markglibres@gmail.com>
2018-03-29 09:53:44 +1100

    rename to timings from businesshours

---
+++ src/MBran.Timings/App_Plugins/MBran.Timings/js/timings.controller.js
@@ -0,0 +1,54 @@
+﻿angular.module('umbraco')
+    .controller('MBran.Timings.TimingsController', function ($scope) {
+
+        function init() {
+            initOptions();
+            initDefaults();
+            initModel();
+        }
+
+        init();
+
+
+        function initOptions() {
+            $scope.model.hideLabel = true;
+
+        }
+
+        function initModel() {
+            $scope.model.timings = [];
+            
+
+            $scope.model.timings.push({
+                day: {
+                    from: 1, to: 6
+                },
+                time: {
+                    from: {
+                        hour: 9,
+                        minute: 0,
+                        meridian: 'AM'
+                    }
+                }
+            });
+
+            $scope.model.timings.push({
+                day: {
+                    from: 1, to: 6
+                },
+                time: {
+                    from: {
+                        hour: 5,
+                        minute: 0,
+                        meridian: 'PM'
+                    }
+                }
+            });
+        }
+
+        function initDefaults() {
+            $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        }
+
+        console.log('test');
+    });
